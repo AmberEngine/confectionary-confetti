139e1a050e3db267abda28f20129a1eb2ab9a8bc
jg75 <james@amberengine.com>
2017-12-21 16:59:50 -0500

    round 3. fight

--- README.md
+++ README.md
@@ -7,19 +7,40 @@ Confetti
 $ pip install -e git+git@github.com:AmberEngine/confetti.git#egg=confetti
 ```
 
-## Choose a key. Your key will be your application's namespace
+## Choose a key. Your key will be part of the namespacing of your application's parameters.
 ```bash
 $ export CONFETTI_KEY=YourKey
 ```
+Optionally you can override this in the constructor's keyword arguments.
+```python
+    config = Confetti(confetti_key='YourKey')
+```
+The default value will be 'Development' in either case.
 
-## Extend in your application
+## Choose an app name. Your app name will be part of the namespacing of your application's parameters.
+```bash
+$ export CONFETTI_APP=YourApp
+```
+Optionally you can override this in the constructor's keyword arguments.
+```python
+    config = Confetti(confetti_app='YourApp')
+```
+The default value will be the class name in either case.
+
+## A boto3 session will be created from your AWS config and credentials or role or you can override the session.
+```python
+    session = boto3.session.Session()
+    config = Confetti(session=session)
+```
+
+## Or you could just extend Confetti in your application
 ```python
 """Example application."""
 
 from confetti import Confetti
 
 
-class MrCoolIceApp(Confetti):
+class YourApp(Confetti):
     """
     Test example implementation.
 
@@ -27,29 +48,35 @@ class MrCoolIceApp(Confetti):
     """
 
     pass
+
+config = YourApp()
 ```
 
-## Store some parameters (this will probably become a cli of some sort)
-```python
-    config = MrCoolIceApp()
-    parameters = [{
-        'Name': 'APP_URL',
-        'Description': 'The URL',
-        'Value': 'http://www.mrcoolice.com/app',
-        'Type': 'String'
-    }, {
-        'Name': 'APP_KEY',
-        'Description': 'All my passwords and PINs in one parameter',
-        'Value': 'abcde12345',
-        'Type': 'SecureString'
-    }]
+## Store some parameters by importing from a JSON file.
+see: http://boto3.readthedocs.io/en/latest/reference/services/ssm.html#SSM.Client.put_parameter
+```json
+[{
+    'Name': 'APP_URL',
+    'Description': 'The URL',
+    'Value': 'http://www.mrcoolice.com/app',
+    'Type': 'String'
+}, {
+    'Name': 'APP_KEY',
+    'Description': 'All my passwords and PINs in one parameter',
+    'Value': 'abcde12345',
+    'Type': 'SecureString'
+}]
+```
 
-    config.put_parameters(parameters)
+```python
+    config = YourApp()
+
+    config.import_parameters('example.json')
 ```
 
 ## Use your parameters in your application
 ```python
-    config = MrCoolIceApp()
+    config = YourApp()
 
     print(config.APP_URL)
 ```
--- confetti.py
+++ confetti.py
@@ -1,81 +1,141 @@
-"""
-Confetti.
+"""Confetti."""
 
-An example of how an application could leverage AWS KMS and SSM
-as an application parameter vault / configuration manager.
-"""
-
+import json
+import logging
 import os
 
 import boto3
+import botocore
 
 import utils.kms
 
 
 class Confetti(object):
-    """Base class to be extended by the application."""
+    """Base class Confetti can be extended by the application."""
 
-    def __init__(self, **kwargs):
+    def __init__(self, confetti_key=None, confetti_app=None, session=None):
         """Initialize and get application parameters."""
+        # Passing confetti_key as an argument to the constructor
+        # overrides the environment variable CONFETTI_KEY.
+        # The default value is 'Development' in either case.
+        self.confetti_key = confetti_key \
+            if confetti_key \
+            else os.getenv('CONFETTI_KEY', 'Development')
+
+        # Passing confetti_app as an argument to the constructor
+        # overrides the environment variable CONFETTI_KEY.
+        # The default value is the class name in either case.
+        self.confetti_app = confetti_app \
+            if confetti_app \
+            else os.getenv('CONFETTI_APP', self.__class__.__name__)
+
+        # Override the default session by supplying a custom session
+        # to the constructor.
+        self.session = session if session else boto3.session.Session()
+
         # Protect these with a leading "_" so they don't interfere with
         # potential parameter names.
         # Also you probably don't want people interacting with these.
-        self._key = kwargs.get('key', os.getenv('CONFETTI_KEY', 'Development'))
-        self._key_id = 'alias/{}'.format(self._key)
-        self._path = '/{}/{}'.format(self._key, self.__class__.__name__)
-        self._session = kwargs.get('session', boto3.session.Session())
-        self._kms = self._session.client('kms')
-        self._ssm = self._session.client('ssm')
-        self.parameters = dict()
+        self._path = '/{}/{}'.format(self.confetti_key, self.confetti_app)
+        self._kms = self.session.client('kms')
+        self._ssm = self.session.client('ssm')
 
-        response = self._ssm.get_parameters_by_path(
-            Path=self._path,
-            WithDecryption=True
-        )
-
-        for parameter in response['Parameters']:
-            name = parameter['Name'].replace(self._path + '/', '')
-
-            self.parameters[name] = parameter["Value"]
+        self.get_parameters()
 
     def __getattr__(self, attribute):
-        """Override __getattr__ to return None on empty parameters."""
-        # When we attempt to get an attribute that doesn't exist on the object,
-        # instead we're actually trying to get it from self.parameters
-        return self.parameters.get(attribute)
+        """
+        Override getattr.
+
+        When we attempt to get an attribute that doesn't exist on the object,
+        instead we're actually trying to get it from self.parameters
+        """
+        return self.parameters.get(attribute, None)
 
     def __getitem__(self, key):
-        """Perform getitem on parameters retrieved from the store."""
-        # If we're holding on to the parameters as a dictionary it makes
-        # key-retrieval easy!
+        """
+        Override getitem.
+
+        Parameters are retrieved from the dictionary.
+        """
         return self.parameters[key]
 
     def __iter__(self):
-        """Iterate over parameters retrieved from kms."""
-        # If we have a dictionary as the storage for the parameters we can
-        # let people iterate on the storage keys too!
+        """
+        Override iter.
+
+        Parameters are iterable.
+        """
         return dict(self.parameters)
 
     def __repr__(self):
-        """Override __repr__ with a representation of self."""
-        return "{}(path='{}', session={})".format(
-            self.__class__.__name__,
-            self._path,
-            self._session
+        """
+        Override repr.
+
+        Create a string representation that is unambiguous so that
+        eval(repr(Confetti(**parameters))) == Confetti(**parameters)
+        """
+        return "{}(confetti_key='{}', confetti_app='{}', session={})".format(
+            self.confetti_key,
+            self.confetti_app,
+            self.session
         )
 
     def __str__(self):
         """Get the string value of self."""
         return repr(self)
 
+    def get_logger(self):
+        """Get a logger."""
+        return logging.getLogger(self.confetti_app)
+
+    def get_parameters(self):
+        """Get parameters from AWS Systems Manager parameter store."""
+        self.parameters = dict()
+
+        for parameter in self._ssm.get_parameters_by_path(
+            Path=self._path,
+            WithDecryption=True
+        ).get('Parameters'):
+            name = parameter['Name'].replace(self._path + '/', '')
+
+            self.parameters[name] = parameter["Value"]
+
     def put_parameters(self, parameters):
         """Put parameters to AWS Systems Manager parameter store."""
+        key_id = 'alias/{}'.format(self.confetti_key)
+        description = self.confetti_key
+        message = '{}: {}: {}'
+        logger = self.get_logger()
+
+        utils.kms.ensure_key(self._kms, key_id, description)
+
         for parameter in parameters:
             if not parameter['Name'].startswith(self._path):
                 parameter['Name'] = '/'.join([self._path, parameter['Name']])
 
             if parameter['Type'] == 'SecureString':
-                parameter['KeyId'] = self._key_id
+                parameter['KeyId'] = key_id
 
-            utils.kms.ensure_key(self._kms, self._key_id, self._key)
-            self._ssm.put_parameter(**parameter)
+            try:
+                self._ssm.put_parameter(**parameter)
+            except botocore.exceptions.ClientError as e:
+                if e.response['Error']['Code']:
+                    logger.warning(
+                        message.format(
+                            e.response['Error']['Code'],
+                            parameter['Name'],
+                            e.response['Error']['Message']
+                        )
+                    )
+                else:
+                    raise e
+
+    def import_parameters(self, json_file):
+        """Import parameters from a json file."""
+        parameters = []
+
+        with open(json_file) as in_file:
+            parameters = json.load(in_file)
+
+        if parameters:
+            self.put_parameters(parameters)
