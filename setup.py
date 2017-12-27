26a820d277e19e8834d6d98b87e3331ba8b5affe
jg75 <james@amberengine.com>
2017-12-27 17:00:49 -0500

    strip pip

--- setup.py
+++ setup.py
@@ -1,21 +1,15 @@
 """Confetti."""
-import os
-
-from pip.req import parse_requirements
 from setuptools import setup, find_packages
 
-requirements = os.path.join(os.path.dirname(__file__), 'requirements.txt')
-install_requires = [
-    str(r.req) for r in parse_requirements(requirements, session=False)
-]
+install_requires = ['boto3==1.5']
+tests_require = install_requires + ['pytest==3.2.3']
 
 setup(
     name='confectionary-confetti',
     version='0.1',
     description=__doc__,
     packages=find_packages(),
+    py_modules=['confetti'],
     install_requires=install_requires,
-    py_modules=[
-        'confetti'
-    ]
+    tests_require=tests_require
 )
