3b839affd5a416ab34c0f2d20f791c4e6a72ca34
jg75 <james@amberengine.com>
2018-01-12 23:16:01 -0500

    fix requirements

--- setup.py
+++ setup.py
@@ -1,15 +1,75 @@
 """Confetti."""
-from setuptools import setup, find_packages
+import os
+import sys
 
-install_requires = ['boto3>=1.5']
-tests_require = install_requires + ['pytest==3.2.3']
+from pip import req
+from setuptools import setup
 
-setup(
-    name='confectionary-confetti',
-    version='0.1',
-    description=__doc__,
-    packages=find_packages(),
-    py_modules=['confetti'],
-    install_requires=install_requires,
-    tests_require=tests_require
-)
+
+def get_app_directory():
+    """Get the location of this application."""
+
+    directory = os.path.dirname(__file__) 
+
+    if not directory:
+        directory = os.getcwd()
+
+    return directory
+
+
+def get_requirements(file_name='requirements.txt'):
+    """Get requirements as a list."""
+
+    path = os.path.join(get_app_directory(), file_name)
+    requirements_list = []
+
+    if os.path.exists(path):
+        requirements_list = [str(r.req) for r in req.parse_requirements(
+            path,
+            session=False
+        )]
+
+    if 'develop' in sys.argv and file_name == 'requirements.txt':
+        requirements_list += get_requirements(
+            file_name='requirements.develop.txt'
+        )
+
+    return requirements_list
+
+
+def get_dependency_links(file_name='dependency_links.txt'):
+    """Get the dependency links as a list."""
+
+    path = os.path.join(get_app_directory(), file_name)
+    dependency_links = []
+
+    if os.path.exists(path):
+        with open(os.path.join(path)) as in_file:
+            dependency_links = [line.strip() for line in in_file]
+
+    return dependency_links
+
+
+def get_description(file_name='README.md'):
+    """Get contents of a file as a string or empty string if none exists."""
+
+    path = os.path.join(get_app_directory(), file_name)
+    description = ""
+
+    if os.path.exists(path):
+        with open(path) as in_file:
+            long_description = in_file.read()
+
+    return description
+
+arguments = {
+    'name': 'confectionary-confetti',
+    'version': '0.1',
+    'license': 'Other/Proprietary License',
+    'long_description': get_description(),
+    'install_requires': get_requirements(),
+    'dependency_links': get_dependency_links(),
+    'py_modules': ['confetti'],
+}
+
+setup(**arguments)
