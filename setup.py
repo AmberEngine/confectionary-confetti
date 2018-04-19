8811c758891cf4a5b94cd0cfaa1e28fe555e0cb8
jg75 <james@amberengine.com>
2018-04-19 14:00:11 -0400

    fix for transmog, steve and others

--- setup.py
+++ setup.py
@@ -3,40 +3,19 @@ import os
 from setuptools import setup, find_packages
 
 
-def get_app_directory():
-    """Get the location of this application."""
-
-    directory = os.path.dirname(__file__)
-
-    if not directory:
-        directory = os.getcwd()
-
-    return directory
-
-
-def get_dependency_links(file_name='dependency_links.txt'):
-    """Get the dependency links as a list."""
-
-    path = os.path.join(get_app_directory(), file_name)
-    dependency_links = []
-
-    if os.path.exists(path):
-        with open(os.path.join(path)) as in_file:
-            dependency_links = [line.strip() for line in in_file]
-
-    return dependency_links
-
-
 def get_description(file_name='README.md'):
-    """Get contents of a file as a string or empty string if none exists."""
+    """Get contents of a file as a string or empty string if none exists.
 
-    path = os.path.join(get_app_directory(), file_name)
+    :param file_name: The name of the file containing the description
+    """
+    path = os.path.join(os.path.dirname(__file__), file_name)
+    description = ""
 
     if os.path.exists(path):
         with open(path) as in_file:
-            return in_file.read()
+            long_description = in_file.read()
 
-    return None
+    return description
 
 
 arguments = {
@@ -45,10 +24,6 @@ arguments = {
     'description': __doc__,
     'long_description': get_description(),
     'license': 'Other/Proprietary License',
-    'install_requires': [
-        'boto3>=1.5',
-    ],
-    'dependency_links': get_dependency_links(),
     'packages': find_packages(),
 }
 
